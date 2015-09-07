74f7e0f0eaf04fba1b0459b93cbe344550b80289
zgs225 <zgs225@gmail.com>
2015-09-07 17:49:58 +0800

    Initial Commit

---
+++ XTHUD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XTHUD
+//
+//  Created by 何振东 on 15/9/7.
+//  Copyright © 2015年 LZA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XTHUD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XTHUD
+//
+//  Created by 何振东 on 15/9/7.
+//  Copyright © 2015年 LZA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
